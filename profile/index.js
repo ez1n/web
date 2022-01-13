51cd30a27522d9dee485a89dad37e179579722d9
unknown <eodrmfdl1004@naver.com>
2022-01-14 01:05:07 +0900

    update my profile

--- profile/index.js
+++ profile/index.js
@@ -42,15 +42,15 @@ btn_favorite_things_open.addEventListener('click', () => {
     </h3> <br>
     <div id="family">
       <div id=family_members>
-        <img id="img" src="imgaes/family1.jpg" alt=parents title=family1 width="150"> <br> 
+        <img id="img" src="images/family1.jpg" alt=parents title=family1 width="150"> <br> 
         parents
       </div>
       <div id="family_members">
-        <img id="img" src="imgaes/family2.jpg" alt=sister title=famliy2 width="150"> <br>
+        <img id="img" src="images/family2.jpg" alt=sister title=famliy2 width="150"> <br>
         sister
       </div>
       <div id="family_members">
-        <img id="img" src="imgaes/family4.jpg" alt=cousin title=famlily4 width="150"> <br>
+        <img id="img" src="images/family4.jpg" alt=cousin title=famlily4 width="150"> <br>
         cousin
         </div>
       
@@ -62,30 +62,30 @@ btn_favorite_things_open.addEventListener('click', () => {
     </h3> <br>
     <div id="travel">
       <div id="trip">
-        <img id="img" src="imgaes/trip2.jpg" alt=Petite_France title=trip1 width="150">
+        <img id="img" src="images/trip2.jpg" alt=Petite_France title=trip1 width="150">
         <br>Petite France
       </div>
       <div id="trip">
-        <img id="img" src="imgaes/trip3.jpg" alt=Nami_Island title=trip2 width="150">
+        <img id="img" src="images/trip3.jpg" alt=Nami_Island title=trip2 width="150">
         <br>Nami Island
       </div>
       <div id="trip">
-        <img id="img" src="imgaes/trip4.jpg" alt=ChunCheon title=trip3 width="150">
+        <img id="img" src="images/trip4.jpg" alt=ChunCheon title=trip3 width="150">
         <br>ChunCheon
       </div>
     </div>
     <div id="travel">
       <br> <br>
       <div id="trip">
-        <img id="img" src="imgaes/trip1.jpg" alt=Busan title=trip4 height="150">
+        <img id="img" src="images/trip1.jpg" alt=Busan title=trip4 height="150">
         <br>ChunCheon
       </div>
       <div id="trip">
-        <img id="img" src="imgaes/trip6.jpg" alt=JeJu_Island1 title=trip5 width="150">
+        <img id="img" src="images/trip6.jpg" alt=JeJu_Island1 title=trip5 width="150">
         <br>JeJu Island1
       </div>
       <div id="trip">
-        <img id="img" src="imgaes/trip7.jpg" alt=JeJu_Island2 title=trip6 width="150">
+        <img id="img" src="images/trip7.jpg" alt=JeJu_Island2 title=trip6 width="150">
         <br>JeJu Island2
       </div>
     </div>
@@ -96,43 +96,43 @@ btn_favorite_things_open.addEventListener('click', () => {
     </h3> <br>
     <div id="friends">
       <div id="friends_members">
-        <img id="img" src="imgaes/friends3.jpg" alt=Mate title=friends1 width="150">
+        <img id="img" src="images/friends3.jpg" alt=Mate title=friends1 width="150">
         <br>Mate
       </div>
       <div id="friends_members">
-        <img id="img" src="imgaes/friends4.jpg" alt=GELB title=friends2 width="150">
+        <img id="img" src="images/friends4.jpg" alt=GELB title=friends2 width="150">
         <br>GELB
       </div>
       <div id="friends_members">
-       <img id="img" src="imgaes/friends5.jpg" alt=2018_leadership_club title=friends3 width="150">
+       <img id="img" src="images/friends5.jpg" alt=2018_leadership_club title=friends3 width="150">
        <br>2018 leadership club
       </div>
     </div>
     <div id="friends">
       <div id="friends_members">
-        <img id="img" src="imgaes/friends6.jpg" alt=du_hyo_jin title=friends4 width="150">
+        <img id="img" src="images/friends6.jpg" alt=du_hyo_jin title=friends4 width="150">
         <br>du hyo jin
       </div>
       <div id="friends_members">
-        <img id="img" src="imgaes/friends7.jpg" alt=machine_learning title=friends5 width="150">
+        <img id="img" src="images/friends7.jpg" alt=machine_learning title=friends5 width="150">
         <br>machine learning
       </div>
       <div id="friends_members">
-        <img id="img" src="imgaes/friends8.jpg" alt=college_juniors title=friends6 width="150">
+        <img id="img" src="images/friends8.jpg" alt=college_juniors title=friends6 width="150">
         <br>college juniors
       </div>
     </div>
     <div id="friends">
       <div id="friends_members">
-        <img id="img" src="imgaes/friends9.jpg" alt=college_classmate1 title=friends7 width="150">
+        <img id="img" src="images/friends9.jpg" alt=college_classmate1 title=friends7 width="150">
         <br>college classmate1
       </div>
       <div id="friends_members">
-        <img id="img" src="imgaes/friends2.jpg" alt=cheering_club title=friends8 height="150">
+        <img id="img" src="images/friends2.jpg" alt=cheering_club title=friends8 height="150">
         <br>cheering club
       </div>
       <div id="friends_members">
-       <img id="img" src="imgaes/friends1.jpg" alt=title=college_classmate2 friends9 width="150">
+       <img id="img" src="images/friends1.jpg" alt=title=college_classmate2 friends9 width="150">
        <br>college classmate2
       </div>
     </div>
