358ac34ad6023d94686173bdc30203084e0c4358
unknown <eodrmfdl1004@naver.com>
2022-01-13 23:47:53 +0900

    update my profile

--- profile/index.js
+++ profile/index.js
@@ -211,53 +211,53 @@ btn_IU_lyrics.addEventListener("click", () => {
   if (btn_lyrics_open == "close") {
     strawberry_moon_lyrics.innerHTML = `<br> <br>
     <div id="lyrics">
-      달이 익어가니 서둘러 젊은 피야 <br>
-      민들레 한 송이 들고 <br>
-      사랑이 어지러이 떠다니는 밤이야 <br>
-      날아가 사뿐히 이루렴 <br> <br>
-    
-      팽팽한 어둠 사이로 <br>
-      떠오르는 기분 <br>
-      이 거대한 무중력에 혹 휘청해도 <br>
-      두렵진 않을 거야 <br> <br>
-    
-      푸르른 우리 위로 <br>
-      커다란 strawberry moon 한 스쿱 <br>
-      나에게 너를 맡겨볼래 eh-oh <br> <br>
-    
-      바람을 세로질러 <br>
-      날아오르는 기분 so cool <br>
-      삶이 어떻게 더 완벽해 ooh <br>
-      다시 마주하기 어려운 행운이야 <br> <br>
-    
-      온몸에 심장이 뛰어 <br>
-      Oh 오히려 기꺼이 헤매고픈 밤이야 <br>
-      너와 길 잃을 수 있다면 <br> <br>
-    
-      맞잡은 서로의 손으로 <br>
-      출입구를 허문 <br>
-      이 무한함의 끝과 끝 또 위아래로 <br>
-      비행을 떠날 거야 <br> <br>
-    
-      푸르른 우리 위로 <br>
-      커다란 strawberry moon 한 스쿱 <br>
-      나에게 너를 맡겨볼래 eh-oh <br>
-      바람을 세로질러 <br>
-      날아오르는 기분 so cool <br>
-      삶이 어떻게 더 완벽해 ooh <br> <br>
-    
-      놀라워 이보다 <br>
-      꿈같은 순간이 또 있을까 (더 있을까) <br>
-      아마도 우리가 처음 발견한 <br>
-      오늘 이 밤의 모든 것, 그 위로 날아 <br> <br>
-    
-      푸르른 우리 위로 <br>
-      커다란 strawberry moon 한 스쿱 <br>
-      세상을 가져보니 어때 eh-oh <br> <br>
-    
-      바람을 세로질러 <br>
-      날아오르는 기분 so cool <br>
-      삶이 어떻게 더 완벽해 ooh
+      <div id="strawberry_moon_lyrics_part">달이 익어가니 서둘러 젊은 피야</div>
+      <div id="strawberry_moon_lyrics_part">민들레 한 송이 들고</div>
+      <div id="strawberry_moon_lyrics_part">사랑이 어지러이 떠다니는 밤이야</div>
+      <div id="strawberry_moon_lyrics_part">날아가 사뿐히 이루렴</div> <br>
+
+      <div id="strawberry_moon_lyrics_part">팽팽한 어둠 사이로</div>
+      <div id="strawberry_moon_lyrics_part">떠오르는 기분</div>
+      <div id="strawberry_moon_lyrics_part">이 거대한 무중력에 혹 휘청해도</div>
+      <div id="strawberry_moon_lyrics_part">두렵진 않을 거야</div><br>
+
+      <div id="strawberry_moon_lyrics_part">푸르른 우리 위로</div>
+      <div id="strawberry_moon_lyrics_part">커다란 strawberry moon 한 스쿱</div>
+      <div id="strawberry_moon_lyrics_part">나에게 너를 맡겨볼래 eh-oh</div> <br>
+
+      <div id="strawberry_moon_lyrics_part">바람을 세로질러</div>
+      <div id="strawberry_moon_lyrics_part">날아오르는 기분 so cool</div>
+      <div id="strawberry_moon_lyrics_part">삶이 어떻게 더 완벽해 ooh</div>
+      <div id="strawberry_moon_lyrics_part">다시 마주하기 어려운 행운이야</div> <br>
+
+      <div id="strawberry_moon_lyrics_part">온몸에 심장이 뛰어</div>
+      <div id="strawberry_moon_lyrics_part">Oh 오히려 기꺼이 헤매고픈 밤이야</div>
+      <div id="strawberry_moon_lyrics_part">너와 길 잃을 수 있다면</div> <br>
+
+      <div id="strawberry_moon_lyrics_part">맞잡은 서로의 손으로</div>
+      <div id="strawberry_moon_lyrics_part">출입구를 허문</div>
+      <div id="strawberry_moon_lyrics_part">이 무한함의 끝과 끝 또 위아래로</div>
+      <div id="strawberry_moon_lyrics_part">비행을 떠날 거야</div> <br>
+
+      <div id="strawberry_moon_lyrics_part">푸르른 우리 위로</div>
+      <div id="strawberry_moon_lyrics_part">커다란 strawberry moon 한 스쿱</div>
+      <div id="strawberry_moon_lyrics_part">나에게 너를 맡겨볼래 eh-oh</div>
+      <div id="strawberry_moon_lyrics_part">바람을 세로질러</div>
+      <div id="strawberry_moon_lyrics_part">날아오르는 기분 so cool</div>
+      <div id="strawberry_moon_lyrics_part">삶이 어떻게 더 완벽해 ooh</div> <br>
+
+      <div id="strawberry_moon_lyrics_part">놀라워 이보다</div>
+      <div id="strawberry_moon_lyrics_part">꿈같은 순간이 또 있을까 (더 있을까)</div>
+      <div id="strawberry_moon_lyrics_part">아마도 우리가 처음 발견한</div>
+      <div id="strawberry_moon_lyrics_part">오늘 이 밤의 모든 것, 그 위로 날아</div> <br>
+
+      <div id="strawberry_moon_lyrics_part">푸르른 우리 위로</div>
+      <div id="strawberry_moon_lyrics_part">커다란 strawberry moon 한 스쿱</div>
+      <div id="strawberry_moon_lyrics_part">세상을 가져보니 어때 eh-oh</div> <br>
+
+      <div id="strawberry_moon_lyrics_part">바람을 세로질러</div>
+      <div id="strawberry_moon_lyrics_part">날아오르는 기분 so cool</div>
+      <div id="strawberry_moon_lyrics_part">삶이 어떻게 더 완벽해 ooh</div>
     </div>`
     btn_lyrics_open = "open"
     btn_IU_lyrics.innerHTML = "HIDE ∧"
@@ -279,90 +279,90 @@ btn_sj_lyrics.addEventListener("click", () => {
   if (btn_warning_open == "close") {
     warning_lyrics.innerHTML = `<br> <br>
     <div id="lyrics">
-      중요한 걸 잊은 느낌 <br>
-      다가오는 불안함에 <br>
-      더 이상은 걷지 못해 <br>
-      왜 이러지 나 모르겠어 <br> <br>
+      <div id="warning_lyrics_part">중요한 걸 잊은 느낌</div>
+      <div id="warning_lyrics_part">다가오는 불안함에</div>
+      <div id="warning_lyrics_part">더 이상은 걷지 못해</div>
+      <div id="warning_lyrics_part">왜 이러지 나 모르겠어</div> <br>
 
-      예전 같지 않은 눈빛 <br>
-      빛을 잃은듯한 future <br>
-      이건 뭐지 <br>
-      흠 에라 몰라 <br> <br>
+      <div id="warning_lyrics_part">예전 같지 않은 눈빛</div>
+      <div id="warning_lyrics_part">빛을 잃은듯한 future</div>
+      <div id="warning_lyrics_part">이건 뭐지</div>
+      <div id="warning_lyrics_part">흠 에라 몰라</div> <br>
 
-      놀러 가자 저 멀리 <br>
-      나를 찾아 떠나자 <br>
-      길을 잃어버려도 좋아 <br>
-      그게 옳을 테니까 <br> <br>
+      <div id="warning_lyrics_part">놀러 가자 저 멀리</div>
+      <div id="warning_lyrics_part">나를 찾아 떠나자</div>
+      <div id="warning_lyrics_part">길을 잃어버려도 좋아</div>
+      <div id="warning_lyrics_part">그게 옳을 테니까</div> <br>
 
-      멀리 찾아 떠나자 <br>
-      나를 잡지 못하게 <br>
-      힘을 내 สู้ๆนะ <br>
-      加油 cheer up baby <br>
-      ¡Ánimo! 頑張って。 <br>
-      더 이상 I cannot go on <br> <br>
+      <div id="warning_lyrics_part">멀리 찾아 떠나자</div>
+      <div id="warning_lyrics_part">나를 잡지 못하게</div>
+      <div id="warning_lyrics_part">힘을 내 สู้ๆนะ</div>
+      <div id="warning_lyrics_part">加油 cheer up baby</div>
+      <div id="warning_lyrics_part">¡Ánimo! 頑張って。</div>
+      <div id="warning_lyrics_part">더 이상 I cannot go on</div> <br>
 
-      Warning! Warning! Warning! <br> <br>
+      <div id="warning_lyrics_part">Warning! Warning! Warning!</div> <br>
 
-      날아가자 높이 <br>
-      많은 걸 볼수록 확실할 테니 <br>
-      멀리 놓을수록 가벼워져 갈 테니까 <br> <br>
+      <div id="warning_lyrics_part">날아가자 높이</div>
+      <div id="warning_lyrics_part">많은 걸 볼수록 확실할 테니</div>
+      <div id="warning_lyrics_part">멀리 놓을수록 가벼워져 갈 테니까</div> <br>
 
-      Warning! 가보자 <br>
-      모든 건 너도 나도 처음일 테니까 <br>
-      Warning! 해보자 <br>
-      아니면 다시 돌아오지 까짓 거 뭐 <br> <br>
+      <div id="warning_lyrics_part">Warning! 가보자</div>
+      <div id="warning_lyrics_part">모든 건 너도 나도 처음일 테니까</div>
+      <div id="warning_lyrics_part">Warning! 해보자</div>
+      <div id="warning_lyrics_part">아니면 다시 돌아오지 까짓 거 뭐</div> <br>
 
-      Now every time I close my eyes <br>
-      떠올라 수만 가지 things <br>
-      어린 시절에 그린 <br>
-      나의 미래에 scene S <br>
-      이루어진 건 잘 없지만 <br>
-      그래도 그때만큼은 즐거웠지 나는 <br>
-      여전하길 비네 <br>
-      시간이 지난 뒤에도 <br>
-      아무렇지 않게 <br>
-      날았으면 해 저 위에 <br>
-      Look where we at 구름 위에 불안감들 비워내 <br>
-      잠시나마 여길 떠나 멀리 warning! <br> <br>
+      <div id="warning_lyrics_part">Now every time I close my eyes</div>
+      <div id="warning_lyrics_part">떠올라 수만 가지 things</div>
+      <div id="warning_lyrics_part">어린 시절에 그린</div>
+      <div id="warning_lyrics_part">나의 미래에 scene S</div>
+      <div id="warning_lyrics_part">이루어진 건 잘 없지만</div>
+      <div id="warning_lyrics_part">그래도 그때만큼은 즐거웠지 나는</div>
+      <div id="warning_lyrics_part">여전하길 비네</div>
+      <div id="warning_lyrics_part">시간이 지난 뒤에도</div>
+      <div id="warning_lyrics_part">아무렇지 않게</div>
+      <div id="warning_lyrics_part">날았으면 해 저 위에</div>
+      <div id="warning_lyrics_part">Look where we at 구름 위에 불안감들 비워내</div>
+      <div id="warning_lyrics_part">잠시나마 여길 떠나 멀리 warning!</div> <br>
 
-      걸어가자 천천히 <br>
-      넘어지지 않도록 <br>
-      조금 느리더라도 좋아 <br>
-      볼 게 많을 테니까 <br> <br>
+      <div id="warning_lyrics_part">걸어가자 천천히</div>
+      <div id="warning_lyrics_part">넘어지지 않도록</div>
+      <div id="warning_lyrics_part">조금 느리더라도 좋아</div>
+      <div id="warning_lyrics_part">볼 게 많을 테니까</div> <br>
 
-      잠시 멈춰 서보자 <br>
-      돌아볼 수 있도록 <br>
-      괜찮아 잘했어 <br>
-      내가 뭐 어쩔 거야 <br>
-      세상은 운이야 <br> <br>
+      <div id="warning_lyrics_part">잠시 멈춰 서보자</div>
+      <div id="warning_lyrics_part">돌아볼 수 있도록</div> 
+      <div id="warning_lyrics_part">괜찮아 잘했어</div>
+      <div id="warning_lyrics_part">내가 뭐 어쩔 거야</div>
+      <div id="warning_lyrics_part">세상은 운이야</div> <br>
 
-      날아가자 높이 <br>
-      많은 걸 볼수록 확실할 테니 <br>
-      멀리 놓을수록 가벼워져 갈 테니까 <br> <br>
+      <div id="warning_lyrics_part">날아가자 높이</div> 
+      <div id="warning_lyrics_part">많은 걸 볼수록 확실할 테니</div> 
+      <div id="warning_lyrics_part">멀리 놓을수록 가벼워져 갈 테니까</div> <br>
 
-      Warning! 떠나자 <br>
-      이대론 너도 나도 불안할 테니까 <br>
-      Warning! 나 혼자면 어때 <br>
-      가면 누구라도 있겠지 뭐 <br> <br>
+      <div id="warning_lyrics_part">Warning! 떠나자</div>
+      <div id="warning_lyrics_part">이대론 너도 나도 불안할 테니까</div>
+      <div id="warning_lyrics_part">Warning! 나 혼자면 어때</div> 
+      <div id="warning_lyrics_part">가면 누구라도 있겠지 뭐</div> <br> 
 
-      무서웠던 맘이 사라지기를 <br>
-      내일이 오지 않길 기도하던 <br>
-      나의 세상도 <br> <br>
+      <div id="warning_lyrics_part">무서웠던 맘이 사라지기를</div> 
+      <div id="warning_lyrics_part">내일이 오지 않길 기도하던</div>
+      <div id="warning_lyrics_part">나의 세상도</div> <br>
 
-      나아갈 거야 앞으로 <br>
-      우울을 밟고서 <br> <br>
+      <div id="warning_lyrics_part">나아갈 거야 앞으로</div>
+      <div id="warning_lyrics_part">우울을 밟고서</div> <br>
 
-      Worth it! Worth it! Worth it! <br> <br>
+      <div id="warning_lyrics_part">Worth it! Worth it! Worth it!</div> <br> 
 
-      나아가자 높이 <br>
-      모든 걸 밟고 찬란한 우주로 <br>
-      멀리 쏘아 올린 작은 별을 다시 찾자 <br> <br>
+      <div id="warning_lyrics_part">나아가자 높이</div> <br>
+      <div id="warning_lyrics_part">모든 걸 밟고 찬란한 우주로</div>
+      <div id="warning_lyrics_part">멀리 쏘아 올린 작은 별을 다시 찾자</div> <br>
 
-      Worth it! <br>
-      가보자 까짓 거 지금보단 나아질 테니까 <br>
-      Worth it! <br>
-      물러서 있기엔 <br>
-      너무 아름다운 나이니까
+      <div id="warning_lyrics_part">Worth it!</div>
+      <div id="warning_lyrics_part">가보자 까짓 거 지금보단 나아질 테니까</div>
+      <div id="warning_lyrics_part">Worth it!</div>
+      <div id="warning_lyrics_part">물러서 있기엔</div> 
+      <div id="warning_lyrics_part">너무 아름다운 나이니까</div>
     </div>`
     btn_warning_open = "open"
     btn_sj_lyrics.innerHTML = "HIDE ∧"
